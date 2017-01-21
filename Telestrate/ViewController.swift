d6db0ac0d65ed80912f6b2c0b35d73e3809b4884
Benjamin Ray Walker <brw.terror@me.com>
2017-01-21 15:44:39 -0700

    Initial Commit

---
+++ Telestrate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Telestrate
+//
+//  Created by Benjamin Ray Walker on 1/21/17.
+//  Copyright © 2017 Benjamin Ray Walker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
