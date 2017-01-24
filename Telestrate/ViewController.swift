9ff28d24a49fb5a8b0e80476dddc4420abc7894f
brw59 <learn.wisdom@icloud.com>
2017-01-23 20:50:34 -0700

    I think our merge issues are actually happening now ... that's good

--- Telestrate/ViewController.swift
+++ Telestrate/ViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // change 8:50
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    // change 8:50
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -27,6 +29,7 @@ class ViewController: UIViewController {
     
     // Jan 23rd, on Monday at 8:35pm test
     // Jan 23rd, on Monday at 8:42pm test
+    // change 8:50
 
 }
 
